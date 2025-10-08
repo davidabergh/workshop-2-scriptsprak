862369ee34819564ba6e14c8020610f6181e5ce0
davidabergh <davidabergh@gmail.com>
2025-10-08 16:31:07 +0200

    Added the analysis text

--- write-to-csv.py
+++ write-to-csv.py
@@ -1,2 +1,9 @@
 import csv
 
+with open('network_incidents.csv', encoding='utf-8') as f:
+    incidents = list(csv.DictReader(f))
+
+report = "INCIDENT ANALYSIS - SEPTEMBER 2024"
+
+with open('incident_analysis.txt', 'w', encoding='utf-8') as f:
+    f.write(report)
